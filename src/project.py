11177230f0c90d36ff8dda632ddb7b0e1322a92c
Rodney Tolbert <thecoolkidrt@gmail.com>
2025-11-14 03:50:35 -0600

    Build: created a window
    Titled the window "Zodiac Legends Title Screen", and filled the screen black

--- src/project.py
+++ src/project.py
@@ -1,4 +1,5 @@
-import
+import pygame
+from PIL import Image
 
 
 class SomeClass():
@@ -15,8 +16,21 @@ class SomeClass():
 
 
 def main():
-    ...
+    pygame.init()
+    pygame.display.set_caption("Zodiac Legends Title Screen")
 
+    resolution = (800, 600)
+    screen = pygame.display.set_mode(resolution)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    
+        black = pygame.Color(0, 0, 0)
+        screen.fill(black)
+        pygame.display.flip()
+    pygame.quit()
 
 def function_1():
     ...
